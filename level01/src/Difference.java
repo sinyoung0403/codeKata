34b045cfd8c9a7347fd8d2f98c248f7daea99464
sinyoung0403 <tlstjs753@gmail.com>
2025-03-04 10:34:05 +0900

    두ì 25.03.04-수의 차

---
+++ level01/src/Difference.java
@@ -0,0 +1,9 @@
+public class Difference {
+  public static int solution(int num1, int num2) {
+    return num1 - num2;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(solution(3, 4));
+  }
+}
