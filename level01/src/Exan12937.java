85148274c7187ddee27cea0f87b1a74fc9000915
sinyoung0403 <tlstjs753@gmail.com>
2025-03-05 10:00:01 +0900

    25.03.05-12937 짝수와 홀수

---
+++ level01/src/Exan12937.java
@@ -0,0 +1,12 @@
+public class Exan12937 {
+  public String solution(int num) {
+    String answer = "";
+    if(num%2==0){
+      answer = "Even";
+    }
+    else {
+      answer = "Odd";
+    }
+    return answer;
+  }
+}
