9c9086c009e7132c1eee53d5bc80314fa6cde400
sinyoung0403 <tlstjs753@gmail.com>
2025-03-05 10:00:24 +0900

    25.03.05-12931 자릿수 더하기

---
+++ level01/src/Exam12931.java
@@ -0,0 +1,11 @@
+public class Exam12931 {
+  public int solution(int n) {
+    int answer = 0;
+    String str = String.valueOf(n);
+    for (int i =0; i< str.length() ; i++){
+      int num = Character.getNumericValue(str.charAt(i));
+      answer += num;
+    }
+    return answer;
+  }
+}
