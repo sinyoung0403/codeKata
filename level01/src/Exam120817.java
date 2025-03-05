a298923d156e1ba91f9dbe638079131567b5d462
sinyoung0403 <tlstjs753@gmail.com>
2025-03-05 09:57:45 +0900

    25.03.05-120817 배열의 평균 값

---
+++ level01/src/Exam120817.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Exam120817 {
+  public double solution(int[] numbers) {
+    double answer = 0;
+    int a= Arrays.stream(numbers).reduce(0,(s1, s2)-> s1+s2);
+    return (double)a/numbers.length;
+  }
+}
