47299f548e2c0e97c003c1660b6ed6b137c38802
sinyoung0403 <tlstjs753@gmail.com>
2025-03-05 09:58:10 +0900

    25.03.05-12944 평균구하기

---
+++ level01/src/Exam12944.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Exam12944 {
+  public double solution(int[] arr) {
+    double answer = 0;
+    int sum = Arrays.stream(arr).reduce(0,(s1, s2)-> s1+s2);
+    return (double)sum/arr.length;
+  }
+}
