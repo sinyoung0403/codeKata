fa0302c4d349df1d3e23c1ad4fc66e356b86e1c0
sinyoung0403 <tlstjs753@gmail.com>
2025-03-06 17:26:59 +0900

    25.03.06-12954 x만큼 간격이 있는 n개의 숫자

---
+++ level01/src/Exam12954.java
@@ -0,0 +1,9 @@
+public class Exam12954 {
+  public long[] solution(int x, int n) {
+    long[] answer = new long[n];
+    for (int i=1; i<=n; i++){
+      answer[i-1] = (long)i*x;
+    }
+    return answer;
+  }
+}
