eeb5fb314738e110fa61fff25063ddcf4b1e53b7
sinyoung0403 <tlstjs753@gmail.com>
2025-03-06 17:25:52 +0900

    25.03.06-12934 정수 제곱근 판별

---
+++ level01/src/Exam12934.java
@@ -0,0 +1,12 @@
+public class Exam12934 {
+  class Solution {
+    public long solution(long n) {
+      Double sqrt = Math.sqrt(n);
+      if(sqrt%1 == 0){
+        return (long) ((sqrt + 1) * (sqrt + 1));
+      } else {
+        return -1;
+      }
+    }
+  }
+}
