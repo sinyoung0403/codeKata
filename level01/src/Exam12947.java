64e430c56a13e151ada8a98f596af2f23d26d053
sinyoung0403 <tlstjs753@gmail.com>
2025-03-10 10:18:22 +0900

    25.03.10-12947 하샤드 수

---
+++ level01/src/Exam12947.java
@@ -0,0 +1,14 @@
+public class Exam12947 {
+  public boolean solution(int x) {
+    String text =  Integer.toString(x);
+    int sum = 0;
+    boolean answer = false;
+    for (int i = 0; i<text.length(); i++){
+      sum += Character.getNumericValue(text.charAt(i));
+    }
+    if(x % sum == 0){
+      answer = true;
+    }
+    return answer;
+  }
+}
