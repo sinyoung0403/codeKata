68296b1d7d50ccba7daf88e7dac66db3be744757
sinyoung0403 <tlstjs753@gmail.com>
2025-03-12 09:39:23 +0900

    25.03.12- 134240 푸드 파이트 대회

---
+++ level3/src/Exam134240.java
@@ -0,0 +1,11 @@
+public class Exam134240 {
+  public String solution(int[] food) {
+    String answer = "";
+    for (int i=1; i<food.length; i++) {
+      if (food[i]/2 != 0) {
+        answer += (String.valueOf(i)).repeat(food[i]/2);
+      }
+    }
+    return answer+"0"+new StringBuilder(answer).reverse().toString();
+  }
+}
