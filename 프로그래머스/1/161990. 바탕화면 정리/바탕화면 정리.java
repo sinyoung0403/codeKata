decb23278d72bd078257ca586575481fb8dfe97f
sinyoung0403 <94594402+sinyoung0403@users.noreply.github.com>
2025-03-20 10:00:13 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.43 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        int minX = 50;
+        int maxX = 0;
+        int minY = 50;
+        int maxY = 0;
+    
+        for (int i=0; i < wallpaper.length; i++){
+            if (wallpaper[i].contains("#")) {
+                for (int j=0; j < wallpaper[i].length(); j++){
+                    if(wallpaper[i].charAt(j)=='#'){
+                        minX = Math.min(minX,j);
+                        minY = Math.min(minY,i);
+                        maxX = Math.max(maxX,j);
+                        maxY = Math.max(maxY,i);
+                    }
+
+                }
+            }
+        }
+        
+        int answer[] = {minY,minX,maxY+1,maxX+1};
+        return answer;
+    }
+}
