d860bb8d3cf1517fcc16a9ef8d5e14d223593925
sinyoung0403 <94594402+sinyoung0403@users.noreply.github.com>
2025-03-28 09:39:24 +0900

    Update 최대공약수와 최소공배수.java

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,13 +1,12 @@
 class Solution {
     public int[] solution(int n, int m) {
-        int gcd = 1;
+        int[] arr = new int[2];
         for (int i = 1; i <= Math.max(n,m); i++ ) {
             if ( n % i == 0 && m % i == 0) {
-                gcd = i;
+                arr[0] = i;
             }
         }
-        int lcm  = (n*m) / gcd;
-        int[] answer = {gcd,lcm};
-        return answer;
+        arr[1]  = (n*m) / arr[0];
+        return arr;
     }
 }
