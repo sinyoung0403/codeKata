c2953c642aa14c27b2daba1179e65780fb996bc1
sinyoung0403 <94594402+sinyoung0403@users.noreply.github.com>
2025-03-31 10:21:46 +0900

    Update 3진법 뒤집기.java

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -2,6 +2,6 @@ import java.util.*;
 import static java.lang.Integer.parseInt;
 class Solution {
   public static int solution(int n) {
-    return parseInt(String.valueOf(new StringBuffer(Integer.toString(n,3)).reverse()),3);
+    return parseInt(new StringBuffer(Integer.toString(n,3)).reverse().toString(),3);
   }
 }
