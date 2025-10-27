f1203c21c011a64e0afc2a40df203e63c7b3ae64
sinyoung0403 <94594402+sinyoung0403@users.noreply.github.com>
2025-10-27 20:09:43 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.03 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] list = new int[num_list.length];
+        for (int i=0; i<num_list.length; i++) {
+            list[i] = num_list[num_list.length-1-i];
+        }
+        return list;
+    }
+}
