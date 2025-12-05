bed53eacea66397d5b67de677fc6aed6d946781f
sinyoung0403 <94594402+sinyoung0403@users.noreply.github.com>
2025-12-05 21:44:39 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.38 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int max = 0;
+        
+        for (int i=0;i < numbers.length; i++) {
+            for (int j=0; j< numbers.length; j ++) {
+                if (i != j) {
+                    max = Math.max(max, numbers[i] * numbers[j]);
+                }
+            }
+        }
+        
+        return max;
+    }
+}
